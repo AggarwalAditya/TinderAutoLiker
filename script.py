557a0f24b4e018242955224b9fd167b68d40b630
Aditya Aggarwal <adi.adag@gmail.com>
2017-12-23 16:25:09 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,6 +4,6 @@ import time
 k=PyKeyboard()
 time.sleep(5)
 
-for i in range(0,10):
+for i in range(0,50):
     k.tap_key(k.right_key)
-    time.sleep(3)
+    time.sleep(3) # so that Tinder does not consider this a bot
